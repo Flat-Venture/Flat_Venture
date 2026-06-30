abea155072f249169f7bf30a8793898d4c789ac7
onikatashi <llho717@naver.com>
2026-06-30 16:46:24 +0900

    처음부터 다시 시작

--- Assets/_Scripts/ScriptableObjects/AllTraitsData.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "AllTraitsData", menuName = "Scriptable Objects/AllTraitsData")]
-public class AllTraitsData : ScriptableObject
-{
-    public List<TraitData> allTraits;
-}
--- Assets/_Scripts/ScriptableObjects/SoundPack.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "SoundPack", menuName = "Scriptable Objects/SoundPack")]
-public class SoundPack : ScriptableObject
-{
-    public List<SoundData> soundList;
-}
